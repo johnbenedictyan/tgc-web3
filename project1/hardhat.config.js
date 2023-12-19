48cf9d3be6e7caf706c1615286088102be943031
John Benedict Yan <johnbenedictyan@gmail.com>
2023-12-19 14:30:06 +0800

    add hardhat ignition

--- project1/hardhat.config.js
+++ project1/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+
+const INFURA_API_KEY = "KEY";
+const SEPOLIA_PRIVATE_KEY = "YOUR SEPOLIA PRIVATE KEY";
+// https://www.infura.io/
+// https://www.infura.io/faucet/sepolia
+
+// npx hardhat ignition deploy ignition/modules/Apollo.js --network localhost
+// npx hardhat ignition deploy ignition/modules/Apollo.js --network sepolia
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
