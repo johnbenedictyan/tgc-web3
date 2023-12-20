aa00a6668c815ef7cd7bfc48c209b067ad7641c8
John Benedict Yan <johnbenedictyan@gmail.com>
2023-12-20 18:11:57 +0800

    remove redundant task

--- project1/hardhat.config.js
+++ project1/hardhat.config.js
@@ -10,22 +10,6 @@ const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
 // npx hardhat ignition deploy ignition/modules/Apollo.js --network localhost
 // npx hardhat ignition deploy ignition/modules/Apollo.js --network sepolia
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-    const provider = hre.ethers.provider;
-
-    for (const account of accounts) {
-        console.log(
-            "%s (%i ETH)",
-            account.address,
-            hre.ethers.formatEther(
-                // getBalance returns wei amount, format to ETH amount
-                await provider.getBalance(account.address)
-            )
-        );
-    }
-});
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.19",
